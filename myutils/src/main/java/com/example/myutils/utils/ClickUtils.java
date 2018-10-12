ba112821fc348b13011591f9087c3d31765759b6
Haipeng <lihaipeng1007@163.com>
2018-10-12 10:10:01 +0800

    新增工具类

---
+++ myutils/src/main/java/com/example/myutils/utils/ClickUtils.java
@@ -0,0 +1,21 @@
+package com.example.myutils.utils;
+
+/**
+ * Created by Haipeng on 2018/3/19.
+ * 防止重复点击事件工具类
+ */
+public class ClickUtils {
+    // 两次点击按钮之间的点击间隔不能少于1000毫秒
+    private static final int MIN_CLICK_DELAY_TIME = 1000;
+    private static long lastClickTime;
+
+    public static boolean isFastClick() {
+        boolean flag = false;
+        long curClickTime = System.currentTimeMillis();
+        if ((curClickTime - lastClickTime) >= MIN_CLICK_DELAY_TIME) {
+            flag = true;
+        }
+        lastClickTime = curClickTime;
+        return flag;
+    }
+}
---
+++ myutils/src/main/java/com/example/myutils/utils/LogUtils.java
@@ -0,0 +1,42 @@
+package com.example.myutils.utils;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+/**
+ * Created by zheng on 2018/7/30.
+ * 日志
+ */
+
+public class LogUtils {
+
+    static final String TAG = "TAG";
+
+    public static void i(String tag, String msg) {  //信息太长,分段打印
+        if (TextUtils.isEmpty(msg)) {
+            msg = "";
+        }
+        //因为String的length是字符数量不是字节数量所以为了防止中文字符过多，
+        //  把4*1024的MAX字节打印长度改为2001字符数
+        int max_str_length = 2001 - tag.length();
+        //大于4000时
+        while (msg.length() > max_str_length) {
+            Log.i(tag, msg.substring(0, max_str_length));
+            msg = msg.substring(max_str_length);
+        }
+        //剩余部分
+        Log.i(tag, msg);
+
+    }
+
+    public static void e(String msg) {
+        if (msg != null)
+            Log.e(TAG, msg);
+    }
+
+    public static void i(String msg) {
+        if (msg != null)
+            Log.i(TAG, msg);
+    }
+
+}
