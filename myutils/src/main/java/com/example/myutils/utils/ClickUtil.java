424f04b5a74e88a3f1f6e760c5b7bf61b1f4787d
Zheng <zhengjiaovip@163.com>
2019-01-09 14:48:32 +0800

    修改ClickUtils间隔时间为500毫秒

--- myutils/src/main/java/com/example/myutils/utils/ClickUtil.java
+++ myutils/src/main/java/com/example/myutils/utils/ClickUtil.java
@@ -6,8 +6,8 @@ package com.example.myutils.utils;
  */
 public class ClickUtil {
 
-    // 两次点击按钮之间的点击间隔不能少于1000毫秒
-    private static final int MIN_CLICK_DELAY_TIME = 1000;
+    // 两次点击按钮之间的点击间隔不能少于500毫秒
+    private static final int MIN_CLICK_DELAY_TIME = 500;
     private static long lastClickTime;
 
     public static boolean isFastClick() {
